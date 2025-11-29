2b1d644cdbdcaa536111752961e507c27178931d
Zach Lariccia <sadboigoon@gmail.com>
2025-11-29 18:09:52 -0500

    Finished .dmg Build on Mac

--- src/main/java/com/example/scrmbl/Application.java
+++ src/main/java/com/example/scrmbl/Application.java
@@ -8,7 +8,7 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxml = new FXMLLoader(Application.class.getResource("/com/example/scrmbl/view.fxml"));
-        Scene scene = new Scene(fxml.load(), 700, 400);
+        Scene scene = new Scene(fxml.load(), 900, 500);
         stage.setTitle("SCRMBL");
         stage.setScene(scene);
         stage.show();
